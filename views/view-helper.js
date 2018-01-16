9935fa43d1dfe5028e03fcd02a0328ee33340a46
Andrews Anthony <AndrewsAnthonySacred@gmail.com>
2018-01-16 17:22:02 +0200

    reorgonize display house in model - authority, schedule, protocol, program

--- views/view-helper.js
+++ views/view-helper.js
@@ -8,7 +8,7 @@ module.exports = {
                   , R.ifElse(
                       R.compose(R.isEmpty, R.prop('street_new_rus'))
                       , R.juxt([
-                          R.prop('street_rus') 
+                          R.prop('street_rus')
                         , R.always('')
                         , R.prop('name_rus')
                         , R.always(',')
@@ -21,7 +21,7 @@ module.exports = {
                         , R.always(',')
                         , R.prop('building')
                         , R.always('(стр.наз.')
-                        , R.prop('street_rus') 
+                        , R.prop('street_rus')
                         , R.always('')
                         , R.prop('name_rus')
                         , R.always(')')
@@ -32,16 +32,16 @@ module.exports = {
                 R.join(' ')
               , R.flatten
               , R.juxt([R.head, R.compose(R.map(R.compose(R.concat(R.__,'.'), (R.head))), R.tail)])
-              , R.map(R.join('')) 
+              , R.map(R.join(''))
               , R.map(R.juxt([R.compose(R.toUpper, R.head), R.compose(R.toLower, R.tail)]))
               , R.split(' ')
               )
 
-, checkUserRule: (rule, userRulelist) => 
+, checkUserRule: (rule, userRulelist) =>
                     R.any(R.whereEq({name: rule}), userRulelist)
 
-, filterFile: (arr, type) => { 
-    
+, filterFile: (arr, type) => {
+
     if (R.equals('photo', type)) {
       return R.compose(
               R.sort(R.descend(R.nth(0)))
@@ -54,5 +54,10 @@ module.exports = {
     }
 
   }
+
+, groupHouseByDistrictAndSector: R.compose(
+                                  R.map(R.groupBy(R.prop('sector')))
+                                , R.groupBy(R.prop('district'))
+                                )
 
 }
